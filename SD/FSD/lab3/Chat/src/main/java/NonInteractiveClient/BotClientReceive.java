6fef157476a605c1cda349b798f7b36dff506d4b
FlavioMartins93 <flavio_mmm@hotmail.com>
2020-11-24 00:33:19 +0000

    lab3 add bot

---
+++ SD/FSD/lab3/Chat/src/main/java/NonInteractiveClient/BotClientReceive.java
@@ -0,0 +1,37 @@
+package NonInteractiveClient;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
+
+public class BotClientReceive implements Runnable {
+    private SocketChannel socket;
+    private int sleepTime;
+
+    public BotClientReceive(SocketChannel socket, int sleepTime) {
+        this.socket = socket;
+        this.sleepTime = sleepTime;
+    }
+
+    @Override
+    public void run() {
+        ByteBuffer buf = ByteBuffer.allocate(100);
+
+        /* Listen for messages */
+        while(true) {
+            try {
+                Thread.sleep(this.sleepTime);
+
+                this.socket.read(buf);
+
+                buf.flip();
+                String msg = StandardCharsets.UTF_8.decode(buf).toString();
+                System.out.print("After sleeping: "+ this.sleepTime + " | Received: " + msg);
+                buf.clear();
+            } catch (IOException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
